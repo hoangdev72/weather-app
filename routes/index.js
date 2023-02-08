92af0e81a21242cf2b94eb9f350eb545e1bd567b
hoangdev72 <tranviethoang2knd@gmail.com>
2023-02-08 22:21:10 +0700

    feat: add api call to view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Weather app | Home page', user: req.user })
+  let url = process.env.API_KEY + '&q=hanoi'
+  process.env.TZ = 'Asia/Bangkok'
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((weather) => {
+      res.render('index', {
+        title: 'Weather app | Home page',
+        user: req.user,
+        weather: weather,
+      })
+    })
+    .catch((err) => console.error('error:' + err))
 })
 
 module.exports = router
